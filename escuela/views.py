78c354c916657e31c60975799717901d28adad4d
Elias <egomez@informat.com>
2020-01-20 00:27:33 -0300

    inicio proyecto

--- escuela/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-# Create your views here.
-
-class TeatroEscuela(TemplateView):
-    print('ACACACAC')
-    template_name = 'escuela.html'
