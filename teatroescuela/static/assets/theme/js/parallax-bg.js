1da997ad77b868d160122d0134ce20bd7d156e43
Kristopher Gomez <kriz_felipe@MacBook-Pro-de-Kristopher.local>
2020-01-25 16:41:46 -0300

    cambio preloader

--- teatroescuela/static/assets/theme/js/parallax-bg.js
+++ teatroescuela/static/assets/theme/js/parallax-bg.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
 	//Home Image Slider
 	$.backstretch([
 	  "images/bg/1.jpg",
-	  "images/bg/2.jpg",
-	  "images/bg/3.jpg"    
+	  "images/bg/2.jpg"
 	], {duration: 4500, fade: 'slow'});
-	
+
 });
